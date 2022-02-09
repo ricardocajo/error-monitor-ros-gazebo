af8452bfb62d0ceb86f0f8a207a9724aada8176b
Ricardo Cordeiro <82663158+ricardocajo@users.noreply.github.com>
2022-02-09 06:10:02 +0000

    make ros_func_lib a package
    make ros_func_lib a package

--- dsl/compile_language.py
+++ dsl/compile_language.py
@@ -1,6 +1,18 @@
 from utils import Context
+import os
+from pathlib import Path
 
 def compile_ros_py(ctx:Context, node):
     """ Creates a ros package capable of running the associated monitor code"""
-    print("compile")
+    #create ros package? or create file in a given package
+    home = str(Path.home())
+    ros_workspace_directory = os.path.join(home, "ros_workspace/src")
+    test_directory = "teste"
+    path = os.path.join(ros_workspace_directory, test_directory)
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    filename = "teste.py"
+    filepath = os.path.join(path,filename)
+    f = open(filepath, "w")
+
     return True
