bef2290cdfa81de947b5b7f2b2da7b75f991b25a
Ricardo Cordeiro <82663158+ricardocajo@users.noreply.github.com>
2022-02-10 03:41:58 +0000

    compiler thinking
    compiler thinking

--- dsl/compile_language.py
+++ dsl/compile_language.py
@@ -2,26 +2,34 @@ from utils import *
 import os
 from pathlib import Path
 
-def compile_ros_py(ctx: Context, node: Node, file_prefix, emitter=None):
-    """ Creates a python script capable of running the associated monitor code in ROS"""
+def compile_ros_py(ctx: Context, node: Node, emitter=None, file_prefix=None):
+    """ Creates a python script capable of running the associated monitor code in ROS """
+    if emitter is None:
+        emitter = Emitter()
+        emitter << "#! /usr/bin/env python"
+        emitter << "import rospy"
+        emitter << "from ros_func_lib.predicates_func import *"
+        emitter << f"rospy.init_node('{file_prefix}')"
+        emitter << "print(\"Monitoring...\")"
+        compile_ros_py(ctx, node, emitter)
+    elif node.type == 'opargs':
+        #....
+        compile_ros_py(ctx, node.args[0], emitter)
+    else:
+        print("Node type doesn't exist")
+    
+    #I need to see how to handle getting messages synchronized to use in the functions
 
-    emitter = Emitter()
-    emitter << "#! /usr/bin/env python"
-    emitter << "import rospy"
-    emitter << "from ros_func_lib.predicates_func import *"
-    emitter << f"rospy.init_node('{file_prefix}')"
-    emitter << "print(\"Monitoring...\")"
-
-    emitter << "msg = get_topic_msg_modelStates()"
-    emitter << "x = position_x(\"turtlebot3_burger\", msg)"
-    emitter << "y = position_y(\"turtlebot3_burger\", msg)"
+    #emitter << "msg = get_topic_msg_modelStates()"
+    #emitter << "x = position_x(\"turtlebot3_burger\", msg)"
+    #emitter << "y = position_y(\"turtlebot3_burger\", msg)"
     #How to check every frame of the simulation for the always keyword
-    emitter << "while True:"
-    emitter << "    if x < 0.0:"
-    emitter << "        print(\"Rule broken\")"
-    emitter << "        break"
-    emitter << "    if y < 0.0:"
-    emitter << "        print(\"Rule broken\")"
-    emitter << "        break"
+    #emitter << "while True:"
+    #emitter << "    if x < 0.0:"
+    #emitter << "        print(\"Rule broken\")"
+    #emitter << "        break"
+    #emitter << "    if y < 0.0:"
+    #emitter << "        print(\"Rule broken\")"
+    #emitter << "        break"
 
     return emitter.get_code()
--- dsl/language.py
+++ dsl/language.py
@@ -35,9 +35,9 @@ _input = f.read()
 ast = parser.parse(lexer=lexer, input=_input)
 #print(ast)
 verify(Context(),ast)
-code = compile_ros_py(Context(), ast, file_prefix)
-#filename = file_prefix + ".py"
-#filepath = os.path.join(ros_package_dir_path, filename)
-#with open(filepath, "w") as f_out:
-#    f_out.write(code)
+code = compile_ros_py(Context(), ast, file_prefix=file_prefix)
+filename = file_prefix + ".py"
+filepath = os.path.join(ros_package_dir_path, filename)
+with open(filepath, "w") as f_out:
+    f_out.write(code)
 #"""
