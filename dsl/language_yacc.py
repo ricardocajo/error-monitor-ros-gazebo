8be0ed4bd17db8eedaa471aa90d1e8eb72e196df
Ricardo Cordeiro <82663158+ricardocajo@users.noreply.github.com>
2022-02-14 13:40:41 +0000

    grammar finish
    grammar finish

--- dsl/language_yacc.py
+++ dsl/language_yacc.py
@@ -8,8 +8,9 @@ literals = tok.literals
 reserved = tok.reserved
 
 precedence = (
-    ('left','+','-','/','*'), #TODO remove this probably?
     ('nonassoc','GTE','LEE','EQ','DIF','>','<'),
+    ('left','+','-'),
+    ('left','/','*'),
 )
 
 #value{x} missing
@@ -21,8 +22,6 @@ def p_program(p):
         p[0] = Node('program', p[1], p[2])
     else:
         p[0] = Node('program', p[1])
-    print("teste1")
-    print(p[0])
 
 def p_command(p):
     '''command : declaration
@@ -30,22 +29,16 @@ def p_command(p):
                | pattern
                | association'''
     p[0] = Node('command', p[1])
-    print("teste2")
-    print(p[0])
 
 def p_declaration(p):
     '''declaration : DECL NAME TOPIC_NAME msgtype
                    | DECL NAME NAME msgtype'''
     p[0] = Node('declaration', p[1], p[2], p[3], p[4])
-    print("teste3")
-    print(p[0])
 
 def p_model(p):
     '''model : MODEL NAME ':' NAME TOPIC_NAME msgtype
              | MODEL NAME ':' NAME NAME msgtype'''
     p[0] = Node('model', p[1], p[2], p[3], p[4], p[5], p[6])
-    print("teste4")
-    print(p[0])
 
 def p_msgtype(p):
     '''msgtype : NAME
@@ -54,15 +47,21 @@ def p_msgtype(p):
         p[0] = Node('msgtype', p[1], p[2], p[3])
     else:
         p[0] = Node('msgtype', p[1])
-    print("teste5")
-    print(p[0])
 
 def p_association(p):
-    '''association : NAME '=' expression
-                   | NAME '=' comparison'''
+    '''association : NAME '=' expression'''
     p[0] = Node('association', p[1], p[2], p[3])
-    print("teste6")
-    print(p[0])
+
+def p_expression(p):
+    '''expression : operation
+                  | comparison
+                  | operand
+                  | operation '{' number '}'
+                  | operand '{' number '}' '''
+    if len(p) > 2:
+        p[0] = Node('expression', p[1], p[2], p[3], p[4])
+    else:
+        p[0] = Node('expression', p[1])
 
 def p_pattern_4(p):
     '''pattern : ALWAYS '(' paargs ')'
@@ -70,37 +69,48 @@ def p_pattern_4(p):
                | EVENTUALLY '(' paargs ')'
                | NOT '(' paargs ')' '''
     p[0] = Node('pattern_4', p[1], p[2], p[3], p[4])
-    print("teste7")
-    print(p[0])
 
 def p_pattern_6(p):
     '''pattern : AFTER '(' paargs ',' paargs ')' '''
     p[0] = Node('pattern_6', p[1], p[2], p[3], p[4], p[5], p[6])
-    print("teste8")
-    print(p[0])
 
 def p_pattern_7(p):
     '''pattern : '(' paargs ')' UNTIL '(' paargs ')'
-               | '(' paargs ')' IMPLIES '(' paargs ')'
-               | '(' paargs ')' AND '(' paargs ')'
-               | '(' paargs ')' OR '(' paargs ')' '''
+               | '(' paargs ')' IMPLIES '(' paargs ')' '''
     p[0] = Node('pattern_7', p[1], p[2], p[3], p[4], p[5], p[6], p[7])
     print("teste9")
     print(p[0])
 
+def p_patter_multi(p):
+    '''pattern : '(' paargs ')' AND '(' paargs ')' pattsup
+               | '(' paargs ')' OR '(' paargs ')' pattsup'''
+    p[0] = Node('pattern_multi', p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8])
+
+def p_pattsup(p):
+    '''pattsup : AND '(' paargs ')' 
+               | OR '(' paargs ')' 
+               | empty'''
+    if len(p) > 2:
+        p[0] = Node('pattsup', p[1], p[2], p[3], p[4])
+    else:  #TODO maybe this will give an error whiel check None type?
+        pass
+
 def p_pattern_10(p):
     '''pattern : AFTER '(' paargs ',' paargs ')' UNTIL '(' paargs ')' '''
     p[0] = Node('pattern_10', p[1], p[2], p[3], p[4], p[5], p[6], p[7], p[8], p[9], p[10])
-    print("teste10")
-    print(p[0])
 
 def p_paargs(p):
     '''paargs : pattern
               | comparison
               | NAME'''
     p[0] = Node('paargs', p[1])
-    print("teste11")
-    print(p[0])
+
+def p_operation(p):
+    '''operation : expression '+' expression
+                 | expression '-' expression
+                 | expression '/' expression
+                 | expression '*' expression'''
+    p[0] = Node('operation', p[1], p[2], p[3])           
 
 def p_comparison(p):
     '''comparison : expression '>' expression
@@ -110,34 +120,21 @@ def p_comparison(p):
                   | expression EQ expression
                   | expression DIF expression'''
     p[0] = Node('comparison', p[1], p[2], p[3])
-    print("teste12")
+    print("teste13")
     print(p[0])
 
-def p_expression(p):
-    '''expression : expression '+' expression
-                  | expression '-' expression
-                  | expression '*' expression
-                  | expression '/' expression
-                  | expression
-                  | operand
-                  | comparison'''
-    if len(p) > 2:
-        p[0] = Node('expression', p[1], p[2], p[3])
-    else:
-        p[0] = Node('expression', p[1])
-    print("teste12.5")
-    print(p[0])
+def p_number(p):
+    '''number : FLOAT
+              | INTEGER'''
+    p[0] = Node('number', p[1])
 
 def p_operand(p):
     '''operand : func
-               | INTEGER
-               | FLOAT
+               | number
                | NAME
                | bool
                | temporalvalue'''
     p[0] = Node('operand', p[1])
-    print("teste14")
-    print(p[0])
 
 def p_bool(p):
     '''bool : TRUE
@@ -152,20 +149,18 @@ def p_func_onearg(p):
             | VELOCITY NAME
             | LOCALIZATION_ERROR NAME'''
     p[0] = Node('func_onearg', p[1], p[2])
-    print("teste15")
-    print(p[0])
 
 def p_func_twoarg(p):
     '''func : DISTANCE NAME NAME'''
     p[0] = Node('func_twoarg', p[1], p[2], p[3])
-    print("teste16")
-    print(p[0])
 
 def p_temporalvalue(p):
     '''temporalvalue : '@' '{' NAME ',' INTEGER '}' '''
-    p[0] = Node('func_twoarg', p[1], p[2], p[3], p[4], p[5], p[6])
-    print("teste17")
-    print(p[0])
+    p[0] = Node('temporalvalue', p[1], p[2], p[3], p[4], p[5], p[6])
+
+def p_empty(p):
+    'empty :'
+    pass
 
 def p_error(p):
     print("Syntax error at '%s'. Line number '%d'" % (p.value, p.lineno))
