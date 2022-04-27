838f429ecf91476b6b9451503a89aa9ae72d73b9
Ricardo Cordeiro <82663158+ricardocajo@users.noreply.github.com>
2022-04-27 09:08:33 +0100

    mattermost message and tweaks
    mattermost message and tweaks

--- unit_tests/unit_test.py
+++ unit_tests/unit_test.py
@@ -4,12 +4,16 @@ import json
 import os
 import signal
 import subprocess
+from datetime import date
 from subprocess import PIPE
 from subprocess import Popen
 from subprocess import STDOUT
 
 import requests
 from distutils.dir_util import copy_tree
+
+# This is a percentage
+warning_treshold = 15
 
 # (["command", ...], "rosbag_name", "lauch_file")
 tests = [
@@ -19,11 +23,11 @@ tests = [
         "stop.launch",
     ),
     (
-        ["_timeout_ = 60", "never turtlebot3_burger.position.x < -1"],
+        ["_timeout_ = 60", "always turtlebot3_burger.position.x >= -1"],
         "rosbag2.bag",
         "stop.launch",
     ),
-]
+]  # never turtlebot3_burger.position.x < -1
 ros_workspace_dir = "/home/rcordeiro/ros_workspace"
 ros_workspace_tests_dir = "/home/rcordeiro/ros_workspace/src/test_pkg/src"
 
@@ -35,7 +39,12 @@ copy_tree(
     "/home/rcordeiro/sim_monitor_compiler/unit_tests/launches",
     f"{ros_workspace_tests_dir}/launches",
 )
-results = []
+results = [
+    f"### **Test results for {date.today()}**",
+    "| Test Number  | Output  | Result |",
+    "| :------------ |:---------------:|:-----:|",
+]
+loop_counter, error_counter = 0, 0
 for test in tests:
     with open("test_.txt", "w") as f_out:
         f_out.write("\n".join(test[0]))
@@ -67,14 +76,35 @@ for test in tests:
     )
 
     stdout_test, nothing = p_test.communicate()
-
     os.killpg(os.getpgid(p_bagfile.pid), signal.SIGTERM)
     os.killpg(os.getpgid(p_main.pid), signal.SIGTERM)
 
-    results.append(stdout_test.decode("utf-8"))
+    output = (
+        "\n".join(stdout_test.replace(b"\x1b", b"").decode("utf-8").split("\n")[1:])
+        .replace("\n", "")
+        .replace("[34m", "")
+        .replace("[0m", "")
+        .replace("[31m", "")
+    )
+    emote = ":exclamation:" if output[:5] == "Error" else ":white_check_mark:"
+    loop_counter += 1
+    error_counter += 1 if emote == ":exclamation:" else 0
+    results.append(f"| Test {loop_counter} | {output}| {emote} |")
 
     os.remove("/home/rcordeiro/sim_monitor_compiler/unit_tests/test_.txt")
     os.remove(f"{ros_workspace_tests_dir}/test_.py")
+
+    p_bagfile.wait()
+    p_main.wait()
+
+test_error_value = 100 * error_counter / loop_counter
+test_error_result = (
+    ":warning:" if test_error_value > warning_treshold else ":white_check_mark:"
+)
+
+results.append(
+    f"#### **The test ended with an {str(100 - test_error_value)}% sucess rate. {test_error_result}**",
+)
 
 requests.post(
     "https://chat.lasige.di.fc.ul.pt/hooks/xnzswnyyn3nt7mn3jwucezqzby",
