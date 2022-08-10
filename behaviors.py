98398259297f368b8f0a7d091be1b2f8a9be2d95
Ricardo Cordeiro <82663158+ricardocajo@users.noreply.github.com>
2022-08-10 03:05:44 +0100

    Direction_invert_error
    Direction_invert_error

--- behaviors.py
+++ behaviors.py
@@ -21,10 +21,30 @@ class Auto_docking_error:
             self.cmd_vel_pub.publish(twist_msg)
 
 
+class Direction_invert_error:
+    def __init__(self):
+        print("simulating direction_invert_error_behavior...")
+        self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+        self.direction_invert_error()
+
+    def get_vel(self):
+        return rospy.wait_for_message("/cmd_vel", Twist)
+
+    def direction_invert_error(self):
+        while not rospy.is_shutdown():
+            vel = self.get_vel()
+            if abs(vel.linear.x) > 0.012:
+                twist_msg = Twist()
+                twist_msg = vel
+                twist_msg.angular.z = -vel.angular.z
+                self.cmd_vel_pub.publish(twist_msg)
+
+
 def main():
     rospy.init_node("error_behaviors_node")
     try:
-        auto_docking_error = Auto_docking_error()
+        # auto_docking_error = Auto_docking_error()
+        direction_invert_error = Direction_invert_error()
     except rospy.ROSInterruptException:
         pass
 
