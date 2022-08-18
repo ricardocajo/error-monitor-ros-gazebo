216034f74b10550be5e919d295f794877b5da61e
Ricardo Cordeiro <82663158+ricardocajo@users.noreply.github.com>
2022-08-18 21:06:36 +0100

    Evaluation errors
    Evaluation errors

--- behaviors.py
+++ behaviors.py
@@ -1,4 +1,6 @@
 #! /usr/bin/env python
+"""<remap from="cmd_vel" to="cmd_vel_original"/>
+_timeout_ = 180"""
 from __future__ import annotations
 
 import rospy
@@ -6,21 +8,31 @@ from geometry_msgs.msg import Twist
 
 
 class Auto_docking_error:
+    """decl vel_robot_perception cmd_vel_original Twist.linear.x
+    after vel_robot_perception > 0, never turtlebot3.velocity =={0.005} 0"""
+
     def __init__(self):
         print("simulating auto_docking_error_behavior...")
         self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+        self.twist = Twist()
         self.auto_docking_error()
 
+    def get_vel(self):
+        return rospy.wait_for_message("cmd_vel_original", Twist)
+
     def auto_docking_error(self):
-        twist_msg = Twist()
         while not rospy.is_shutdown():
-            twist_msg.linear.x = 0.0
-            twist_msg.angular.z = 0.0
-            self.cmd_vel_pub.publish(twist_msg)
+            vel = self.get_vel()
+            self.twist = vel
+            if abs(vel.linear.x) < 0.015:
+                self.twist.linear.x = 0.0
+            self.cmd_vel_pub.publish(self.twist)
 
 
-# <remap from="cmd_vel" to="cmd_vel_original"/>
 class Direction_invert_error:
+    """decl angular_vel_robot_perception cmd_vel_original Twist.angular.z
+    after turtlebot3.velocity.angular.z < 0, never angular_vel_robot_perception > 0"""
+
     def __init__(self):
         print("simulating direction_invert_error_behavior...")
         self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
@@ -39,11 +51,34 @@ class Direction_invert_error:
             self.cmd_vel_pub.publish(self.twist)
 
 
+class Backwards_movement_error:
+    """decl vel_robot_perception cmd_vel_original Twist.linear.x
+    after turtlebot3.velocity.linear.x < 0, never vel_robot_perception > 0"""
+
+    def __init__(self):
+        print("simulating backwards_movement_error_behavior...")
+        self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+        self.twist = Twist()
+        self.backwards_movement_error()
+
+    def get_vel(self):
+        return rospy.wait_for_message("cmd_vel_original", Twist)
+
+    def backwards_movement_error(self):
+        while not rospy.is_shutdown():
+            vel = self.get_vel()
+            self.twist = vel
+            if abs(vel.linear.x) > 0.03:
+                self.twist.linear.x = -vel.linear.x
+            self.cmd_vel_pub.publish(self.twist)
+
+
 def main():
     rospy.init_node("error_behaviors_node")
     try:
         # auto_docking_error = Auto_docking_error()
-        direction_invert_error = Direction_invert_error()
+        # direction_invert_error = Direction_invert_error()
+        backwards_movement_error = Backwards_movement_error()
     except rospy.ROSInterruptException:
         pass
 
